9265be9031247e7154c397c4661a7a406caa9bed
Evgeniy Boyko <c00p.g4ming@gmail.com>
2020-08-07 14:53:08 +0300

    laser sight distance

--- Assets/Scripts/GunInstance.cs
+++ Assets/Scripts/GunInstance.cs
@@ -28,6 +28,8 @@ public class GunInstance : MonoBehaviour
     private float m_deltaX;
     private float m_deltaY;
 
+    private float m_sensitivity = 35f;
+
     private void Awake()
     {
         m_lineRenderer = GetComponent<LineRenderer>();
@@ -52,10 +54,30 @@ public class GunInstance : MonoBehaviour
         {
             RaycastHit hit;
 
+            m_testMousePosition = Input.mousePosition;
+            m_testMousePosition.z = 1.0f;
 
-            if (Physics.Raycast(transform.position, m_secondarylaserEndPosition, out hit))
+            m_currentgMousePosition =
+                (m_mainCamera.ScreenToWorldPoint(m_testMousePosition) - m_startingMousePosition) * m_sensitivity;
+            //m_laserEndPosition = new Vector3(m_startingMousePosition.x - m_deltaX, m_startingMousePosition.y - m_deltaY, transform.position.z + 20f);
+            m_currentgMousePosition.z = 0f;
+
+            m_secondarylaserEndPosition = transform.position + new Vector3(0, 0, 20f) + m_currentgMousePosition;
+            Debug.DrawRay(transform.position, m_secondarylaserEndPosition);
+
+            if (Physics.Raycast(transform.position, m_secondarylaserEndPosition, out hit)) //НУЖНО ПЛЯСАТЬ ОТСЮДА
             {
                 m_laserEndPosition = hit.point;
+                //m_testMousePosition = Input.mousePosition;
+                //m_testMousePosition.z = 1.0f;
+
+                //m_currentgMousePosition =
+                //    (m_mainCamera.ScreenToWorldPoint(m_testMousePosition) - m_startingMousePosition) * m_sensitivity;
+                ////m_laserEndPosition = new Vector3(m_startingMousePosition.x - m_deltaX, m_startingMousePosition.y - m_deltaY, transform.position.z + 20f);
+                //m_currentgMousePosition.z = 0f;
+
+                //m_laserEndPosition = hit.point/* + m_currentgMousePosition*/;
+
             }
             else
             {
@@ -87,16 +109,15 @@ public class GunInstance : MonoBehaviour
 
     private void ControlLaserEndPosition()
     {
+        print("isWorking");
         m_testMousePosition = Input.mousePosition;
         m_testMousePosition.z = 1.0f;
 
         m_currentgMousePosition =
-            (m_mainCamera.ScreenToWorldPoint(m_testMousePosition) - m_startingMousePosition) * 40f;
-        //m_laserEndPosition = new Vector3(m_startingMousePosition.x - m_deltaX, m_startingMousePosition.y - m_deltaY, transform.position.z + 20f);
+            (m_mainCamera.ScreenToWorldPoint(m_testMousePosition) - m_startingMousePosition) * m_sensitivity;
         m_currentgMousePosition.z = 0f;
 
         m_laserEndPosition = transform.position + new Vector3(0, 0, 20f) + m_currentgMousePosition;
-        m_secondarylaserEndPosition = m_laserEndPosition;
     }
 
     //m_laserEndPosition = transform.position + (Vector3.forward * m_laserDistance); //just shoot forward
