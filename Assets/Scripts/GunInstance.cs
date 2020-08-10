0a3999271ad3b7c73c95ad309b63bf2c5e315e5f
Evgeniy Boyko <c00p.g4ming@gmail.com>
2020-08-10 12:42:23 +0300

    hook progress

--- Assets/Scripts/GunInstance.cs
+++ Assets/Scripts/GunInstance.cs
@@ -6,38 +6,45 @@ using NaughtyAttributes;
 [RequireComponent(typeof(LineRenderer))]
 public class GunInstance : MonoBehaviour
 {
+
     [BoxGroup("Gun Parameters"), SerializeField] private float m_laserDistance;
 
     [BoxGroup("References"), SerializeField] private Camera m_mainCamera;
     [BoxGroup("References"), SerializeField] private Transform m_rightHand;
-
+    [BoxGroup("References"), SerializeField] private HookInstance m_hook;
 
 
     private LineRenderer m_lineRenderer;
 
     private Vector3 m_laserEndPosition;
     private Vector3 m_secondarylaserEndPosition;
-
-    private bool m_laserActivityState;
-
-
     private Vector3 m_testMousePosition;
     private Vector3 m_currentgMousePosition;
     private Vector3 m_startingMousePosition;
 
     private float m_deltaX;
     private float m_deltaY;
-
     private float m_sensitivity = 40f;
+
+    private int m_grabbingObjectLayer;
+
+    private bool m_laserActivityState;
+
 
     private void Awake()
     {
         m_lineRenderer = GetComponent<LineRenderer>();
+        m_grabbingObjectLayer = LayerMask.NameToLayer("GrabbingObject");
     }
 
     private void Start()
     {
         StartingSetup();
+    }
+    private void StartingSetup()
+    {
+        transform.SetParent(m_rightHand);
+
     }
 
     private void Update()
@@ -61,7 +68,7 @@ public class GunInstance : MonoBehaviour
             m_currentgMousePosition.z = 0f;
             m_secondarylaserEndPosition = /*transform.position + */new Vector3(0, 0, 20f) + m_currentgMousePosition;
 
-            if (Physics.Raycast(transform.position, m_secondarylaserEndPosition, out hit)) //НУЖНО ПЛЯСАТЬ ОТСЮДА
+            if (Physics.Raycast(transform.position, m_secondarylaserEndPosition, out hit))
             {
                 Debug.DrawRay(transform.position, m_secondarylaserEndPosition);
                 m_laserEndPosition = hit.point;
@@ -76,14 +83,13 @@ public class GunInstance : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
-            m_startingMousePosition = Vector3.zero;
+
+
+            ConfirmAimOnTarget();
         }
     }
 
-    private void StartingSetup()
-    {
-        transform.SetParent(m_rightHand);
-    }
+
 
     public IEnumerator EnableLaserSight(bool state, float delay)
     {
@@ -99,4 +105,34 @@ public class GunInstance : MonoBehaviour
     }
 
     //m_laserEndPosition = transform.position + (Vector3.forward * m_laserDistance); //just shoot forward
+
+    private void ConfirmAimOnTarget()
+    {
+        RaycastHit hit;
+        m_testMousePosition = Input.mousePosition;
+        m_testMousePosition.z = 1.0f;
+        m_currentgMousePosition =
+            (m_mainCamera.ScreenToWorldPoint(m_testMousePosition) - m_startingMousePosition) * m_sensitivity;
+        m_currentgMousePosition.z = 0f;
+        m_secondarylaserEndPosition = /*transform.position + */new Vector3(0, 0, 20f) + m_currentgMousePosition;
+
+        if (Physics.Raycast(transform.position, m_secondarylaserEndPosition, out hit))
+        {
+            if (hit.collider.gameObject.layer == m_grabbingObjectLayer)
+            {
+                //enemy was found, we can shoot hook
+                m_hook.m_targetPosition = hit.point;
+                m_hook.m_hookState = HookState.FliesToBase;
+                m_hook.transform.parent = null;
+            }
+        }
+        else
+        {
+            //enemy is not is sight, hook will be short
+        }
+
+        m_startingMousePosition = Vector3.zero;
+    }
+
+
 }
---
+++ Assets/Scripts/HookInstance.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public enum HookState
+{
+    None = 0,
+    Based,
+    FliesToTarget,
+    HitTarget,
+    FliesToBase
+}
+
+public class HookInstance : MonoBehaviour
+{
+    [HideInInspector] public HookState m_hookState;
+    [HideInInspector] public Vector3 m_targetPosition;
+
+    private float m_hookMovementSpeed = 20f;
+
+    private Vector3 m_hookLocalStartPosition;
+
+    private Transform m_parent;
+
+    private const int m_grabbingObjectLayer = 8;
+
+    private void Awake()
+    {
+        m_hookState = HookState.Based;
+        m_hookLocalStartPosition = transform.localPosition;
+        m_parent = gameObject.transform.parent;
+    }
+
+    private void Update()
+    {
+        switch (m_hookState)
+        {
+            case HookState.FliesToBase:
+                {
+                    ShootGrabHookToTarget();
+                    break;
+                }
+            case HookState.FliesToTarget:
+                {
+                    ReturnHookToBase();
+                    break;
+                }
+        }
+    }
+
+    private void ShootGrabHookToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, m_targetPosition, Time.deltaTime * m_hookMovementSpeed);
+    }
+
+    private void ReturnHookToBase()
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.position, m_hookLocalStartPosition, Time.deltaTime * m_hookMovementSpeed * 3f);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.layer)
+        {
+            case m_grabbingObjectLayer:
+                {
+
+                    break;
+                }
+        }
+    }
+
+}
